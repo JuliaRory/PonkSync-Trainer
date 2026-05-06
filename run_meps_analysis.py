b6c60abdbabeba425a7509a0735fe109d9fcfaf1
JuliaRory <shevtsova.jg@gmail.com>
2026-05-06 15:08:46 +0300

    ponk delay check

--- run_meps_analysis.py
+++ run_meps_analysis.py
@@ -7,12 +7,12 @@ seq2 = np.asarray([1,2,1,1,1,2,1,1,1,2,1,1,2,1,1,2,1,1,1,1,2,1,1,1,2,1,2,1,1,1,1
 seq3 = np.asarray([1,1,1,1,1,2,1,1,1,2,1,2,1,1,1,2,1,1,2,1,1,1,1,2,1,1,1,2,1,1,2,1,1,2,1,1,1,1,1,2])
 seq4 = np.asarray([1,2,1,1,1,1,1,1,2,1,1,2,1,1,2,1,1,1,1,2,1,2,1,1,1,2,1,1,2,1,1,1,1,2,1,2,1,1,1,1])
 
-subject = "03AZ"
+subject = "04KK"
 to_analysis = [
-    {"record": "08_03AZ_tms_52MSO_real.hdf", "seq": seq1, "motor_label": "real", "power": "52MSO"},
-    {"record": "09_03AZ_tms_43MSO_real.hdf", "seq": seq2, "motor_label": "real", "power": "43MSO"},
-    {"record": "10_03AZ_tms_43MSO_MI.hdf", "seq": seq3, "motor_label": "MI", "power": "43MSO"},
-    {"record": "11_03AZ_tms_52MSO_MI.hdf", "seq": seq1, "motor_label": "MI", "power": "52MSO"},
+    {"record": "08_04KK_tms_40MSO_real.hdf", "seq": seq1, "motor_label": "real", "power": "40MSO"},
+    {"record": "09_04KK_tms_32MSO_real.hdf", "seq": seq1, "motor_label": "real", "power": "32MSO"},
+    {"record": "10_04KK_tms_32MSO_MI.hdf", "seq": seq3, "motor_label": "MI", "power": "32MSO"},
+    {"record": "11_04KK_tms_40MSO_MI.hdf", "seq": seq4, "motor_label": "MI", "power": "40MSO"},
 ]
 
 
